27fbb8e5b9740566161b48f111d49ed182a37033
Siddharth Singh <siddharths51700@gmail.com>
2020-04-04 23:16:50 +0530

    Login,Signup static pages done!

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-const login = () => {
-  console.log("Inside login component");
+const Signup = () => {
   return (
     <div>
       <div className="limiter">
@@ -16,9 +15,14 @@ const login = () => {
                 />
               </span>
 
+              <div className="wrap-input100">
+                <input className="input100" type="text" name="Name" />
+                <span className="focus-input100" data-placeholder="Name"></span>
+              </div>
+
               <div
                 className="wrap-input100 validate-input"
-                data-validate="Invalid Email Id "
+                data-validate="Invalid Email Id"
               >
                 <input className="input100" type="text" name="email" />
                 <span
@@ -41,19 +45,40 @@ const login = () => {
                 ></span>
               </div>
 
+              <div
+                className="wrap-input100 validate-input"
+                data-validate="Invalid password"
+              >
+                <span className="btn-show-pass">
+                  <i className="zmdi zmdi-eye"></i>
+                </span>
+                <input
+                  className="input100"
+                  type="password"
+                  name="confirmPass"
+                />
+                <span
+                  className="focus-input100"
+                  data-placeholder="Confirm Password"
+                ></span>
+              </div>
+
               <div className="container-login100-form-btn">
                 <div className="wrap-login100-form-btn">
                   <div className="login100-form-bgbtn"></div>
-                  <button className="login100-form-btn vButton4">Login</button>
+                  <button className="login100-form-btn vButton4">
+                    Sign Up
+                  </button>
                 </div>
                 <span className="login100-form-title googleLogin">
                   <img
-                    src="images/Or login with.png"
+                    src="images/Or sign up with.png"
                     className="img-fluid"
                     alt="Responsive image"
                   />
                 </span>
               </div>
+
               <div>
                 <span className="login100-form-title googleLogo">
                   <img
@@ -65,10 +90,10 @@ const login = () => {
               </div>
 
               <div className="text-center p-t-30">
-                <span className="txt1">Don’t have an account?</span>
+                <span className="txt1">Already have an account?</span>
 
-                <a className="txt2" href="signup">
-                  Sign Up
+                <a className="txt2" href="index.html">
+                  Sign In
                 </a>
               </div>
             </form>
@@ -81,4 +106,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default Signup;
