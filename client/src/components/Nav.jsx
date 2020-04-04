168291349ae70d379fa8995f2db4a548ed69a74c
Siddharth Singh <siddharths51700@gmail.com>
2020-04-04 19:56:02 +0530

    Navbar done!

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,18 +4,9 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   const [toggle, fToggle] = useState("active");
   return (
-    <nav id="sidebar" className={toggle}>
+    <nav id="sidebar" className="inactive">
       <div className="custom-menu">
-        <button
-          type="button"
-          id="sidebarCollapse"
-          className="btn btn-primary"
-          onClick={(e) => {
-            if (toggle == "inactive") {
-              fToggle("active");
-            } else fToggle("inactive");
-          }}
-        >
+        <button type="button" id="sidebarCollapse" className="btn btn-primary">
           <i className="fa fa-bars"></i>
           <span className="sr-only">Toggle Menu</span>
         </button>
@@ -26,34 +17,39 @@ const Nav = () => {
             Flash
           </a>
         </h1>
-        <ul className="list-unstyled components mb-5">
+        <div className="account">
+          <div className="account">
+            <img
+              src="images/contactDetection/adult-beard-boy-220453.png"
+              alt="..."
+              className="accountImage"
+            />
+            <span className="fa fa-check-circle mr-3"></span>
+            <span className="mr-3 vText">George Smith</span>
+          </div>
+        </div>
+        <ul className="list-unstyled components mb-5 vList">
           <li className="active">
             <Link to="/adv">
-              <span className="fa fa-home mr-3"></span> Advisories{" "}
+              <span className="fa fa-star mr-3"> </span> Advisories
             </Link>
           </li>
           <li>
             <Link to="/checkContact">
-              <span className="fa fa-user mr-3"></span> Check Contact
+              <span className="fa fa-users mr-3"></span> Check Contact
             </Link>
           </li>
           <li>
             <Link to="/testingEligibility">
-              <span className="fa fa-briefcase mr-3"></span> Testing Eligibility{" "}
+              <span className="fa fa-medkit mr-3"></span> Testing Eligibility{" "}
             </Link>
           </li>
           <li>
             <Link to="/followUp">
-              <span className="fa fa-sticky-note mr-3"></span> Follow up
-            </Link>
-          </li>
-          <li>
-            <Link to="/abt">
-              <span className="fa fa-paper-plane mr-3"></span> About{" "}
+              <span className="fa fa-suitcase mr-3"></span> Follow up
             </Link>
           </li>
         </ul>
-        <div className="footer"></div>
       </div>
     </nav>
   );
